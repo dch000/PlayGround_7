2d5c2d6aa773cd386d4385c6c56da139d34e22d3
dch000 <dmitriy.ev.ch@yandex.ru>
2023-02-12 15:47:55 +0300

    first

---
+++ MyPlayground_7.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+// MARK: Базовые типы данных в swift
+
+//Float and Double
+var fractionalPrice: Double = 149.99
+print(fractionalPrice)
+fractionalPrice = 99.99
+print(fractionalPrice)
+
+//String
+var greetingMessage: String = "Hello"
+print(greetingMessage)
+
+greetingMessage = "Hello World"
+print(greetingMessage)
+
+//Bool
+var isItColdOutside: Bool = true
+print(isItColdOutside)
+
+isItColdOutside = false
+print(isItColdOutside)
+
+
+// MARK: Операторы
+
+//Операторы присвоения
+var price: Int = 100
+
+print(price + 50)
+print(price - 1)
+print(price / 3)
+print(price * 7)
+
+price = price / 3
+
+// Операторы сравнения
+var price1 = 100
+var price2 = 250
+
+
